89f95ad53fd3f904e425ea4ba17ca783699e4276
muhammadz <m@tryfinally.com>
2016-03-17 17:54:40 +0200

    demo commit implement feature #3 solves #43, #67

--- app/src/main/java/course/com/tryfinally/demodemo/MainActivity.java
+++ app/src/main/java/course/com/tryfinally/demodemo/MainActivity.java
@@ -36,9 +36,6 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        if(drawer!=null)
-//        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         //navigationView.setNavigationItemSelectedListener(this);
